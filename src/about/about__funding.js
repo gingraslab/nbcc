5d074cd01c0582d6d5df5a12c3f97435d7146f97
James <jknight@lunenfeld.ca>
2019-08-01 15:58:12 -0400

    Update dependencies

--- src/about/about__funding.js
+++ src/about/about__funding.js
@@ -23,9 +23,9 @@ const Funding = () => (
     </h2>
     <p>
       The Centre is supported by funding from the Canada Foundation for Innovation
-      (CFI), The Ontario Ministry of Economic Development Job Creation and Trade, Genome Canada through
-      Ontario Genomics, Canadian Institutes of Health Research (CIHR), Terry Fox Research
-      Institute and the Lunenfeld-Tanenbaum Research Institute.
+      (CFI), The Ontario Ministry of Economic Development Job Creation and Trade, Genome
+      Canada through Ontario Genomics, Canadian Institutes of Health Research (CIHR),
+      Terry Fox Research Institute and the Lunenfeld-Tanenbaum Research Institute.
     </p>
     <div className="about__funding-images">
       <a href="https://www.innovation.ca/">
